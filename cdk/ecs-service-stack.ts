aea8193e052b3c57c80c5fdfc342bc6d90569402
Casey Lee <caseypl@amazon.com>
2024-11-11 14:07:54 -0800

    wip: gitlab-ci testing

--- cdk/ecs-service-stack.ts
+++ cdk/ecs-service-stack.ts
@@ -1,5 +1,6 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { DefaultStackSynthesizer, Stack, StackProps } from 'aws-cdk-lib';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Cluster, ContainerImage } from 'aws-cdk-lib/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
@@ -12,12 +13,18 @@ export class EcsServiceStack extends Stack {
 
     const cluster = new Cluster(this, 'NestRecipesAppCluster', { vpc });
 
+    const qualifier = this.node.tryGetContext('aws:cdk:qualifier') ?? DefaultStackSynthesizer.DEFAULT_QUALIFIER;
+    const registry = Repository.fromRepositoryName(this, 'CdkAssetRepository', 
+      `cdk-${qualifier}-container-assets-${this.account}-${this.region}`,
+    );
+    const tag = this.node.tryGetContext('tag') ?? 'latest';
+
     new ApplicationLoadBalancedFargateService(this, 'NestRecipesAppService', {
       cluster,
       cpu: 256,
       desiredCount: 1,
       taskImageOptions: {
-        image: ContainerImage.fromAsset('./'),
+        image: ContainerImage.fromEcrRepository(registry, tag),
         containerPort: 3000,
       },
       memoryLimitMiB: 512,
