93d37444327a201641ab70348b88429271d74dd5
Pablo Silva <pcsilva@amazon.com>
2025-02-19 22:05:15 -0300

    Update app.ts

--- cdk/app.ts
+++ cdk/app.ts
@@ -4,6 +4,6 @@ import { PipelineStack } from './pipeline-stack';
 
 const app = new App();
 new PipelineStack(app, 'EcsServicePipeline', {
-  env: { account: '464380571579', region: 'us-east-1' }, // Pipeline will be created in this environment
+  env: { account: '211125471568', region: 'us-east-1' }, // Pipeline will be created in this environment
 });
 app.synth();
